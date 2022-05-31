ebe86fb5b2f780bf08953091867bc7b32ab7b423
Michael Shen <electrycity84@gmail.com>
2022-05-31 18:32:42 +0800

    change the homepage name again

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/photowebsite" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
